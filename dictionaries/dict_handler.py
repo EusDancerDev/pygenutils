9c00996a871680aacb1f9c57356eb8fa6fb274ed
climatechange-tech <jagabantxo@gmail.com>
2024-06-10 21:56:19 +0200

    Update module to manipulate data in dictionaries, and export to or import from JSON files

--- dictionaries/dict_handler.py
+++ dictionaries/dict_handler.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 21:52:48 2024
+
+@author: jonander
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 #----------------#
 # Import modules #
@@ -18,105 +26,161 @@ from strings.string_handler import aux_ext_adder, get_obj_specs
 # Define custom functions #
 #-------------------------#
 
-# File exportations #
-#-------------------#
+# Conversions to JSON files and importations from them #
+#------------------------------------------------------#
 
-def dict2json(dictionary, json_indent=4, out_file_path=None):    
+def dict2json(dictionary, json_indent=4, out_file_path=None):
     """
-    Function that converts a dictionary (not only its content, 
-    but as a whole) to a string and writes the latter to a JSON file.
-    
-    Parameters 
+    Convert a dictionary to a JSON string and write it to a file.
+
+    Parameters
     ----------
     dictionary : dict
-        The original dictionary, no matter what structure is composed of.
-    json_indent : int
-        Determines the space to be used expressing the dictionary 
-        as a string. Default value is that used widely, 
-        a tab, equivalent to 4 whitespaces.
-    out_file_path : str
-        Output file path, could be absolute or relative.
-        If not passed, default name 'dict2json.json' will be used.
-            
+        The dictionary to be converted to JSON format.
+    json_indent : int, optional
+        The number of spaces to use as indentation in the JSON file. Default is 4.
+    out_file_path : str, optional
+        The output file path. If not provided, a default name 'dict2json.json' will be used.
+
     Returns
     -------
-    Output file path in which the whole dictionary is written.    
+    str
+        The output file path where the JSON data is written.
+
+    Raises
+    ------
+    IOError
+        If the file cannot be written to the specified path.
     """
     
-    # Convert dictionary  to string #
+    # Convert dictionary to JSON string #
     dict_str = json.dumps(dictionary, indent=json_indent)
     
-    # Write the string directly and at once to the given path #
-    #---------------------------------------------------------#
-    
-    # Check whether a path is given #
+    # Determine the output file path #
     if out_file_path is None:
         out_file_path = f"dict2json.{extension}"
-        
-    # Check whether the path contains an extension, else add it #
     else:
-        containsPathExtension = len(get_obj_specs(out_file_path, 'ext')) > 0
-        if not containsPathExtension:
+        contains_path_extension = len(get_obj_specs(out_file_path, 'ext')) > 0
+        if not contains_path_extension:
             out_file_path = aux_ext_adder(out_file_path, extension)
 
     try:
-        out_file_obj = open(out_file_path, 'w')        
-    except:
-        raise IOError("Could not write to file '{out_file_path}',"
-                      "invalid path.")
-    else:
-        out_file_obj.write(dict_str)
+        with open(out_file_path, 'w') as out_file_obj:
+            out_file_obj.write(dict_str)
         
-        # Get the file name's parent and the name without the relative path #        
+        # Get file specs #
         out_file_parent = get_obj_specs(out_file_path, obj_spec_key="parent")
         out_file_no_rel_path = get_obj_specs(out_file_path, obj_spec_key="name")
         
-        # Find already existing file #
+        # Check for existing file #
         file_already_exists = (len(find_files_by_globstr(f"*{out_file_path}*", ".")) > 0)
         
         if file_already_exists:
-            overwrite_stdin\
-            = input(f"Warning: file '{out_file_no_rel_path}' "
-                    f"at directory '{out_file_parent}' already exists.\n"
-                    "Do you want to overwrite it? (y/n) ")
-            
-            while (overwrite_stdin != "y" and overwrite_stdin != "n"):
-                overwrite_stdin = input("\nPlease select 'y' for 'yes' "
-                                       "or 'n' for 'no': ")
-            else:    
-                if overwrite_stdin == "y":
-                    out_file_obj.close()
-                else:
-                    pass
+            overwrite_stdin = input(
+                f"Warning: file '{out_file_no_rel_path}' "
+                f"at directory '{out_file_parent}' already exists.\n"
+                "Do you want to overwrite it? (y/n) "
+            )
+            while overwrite_stdin not in ["y", "n"]:
+                overwrite_stdin = input("\nPlease select 'y' for 'yes' or 'n' for 'no': ")
+            if overwrite_stdin == "y":
+                pass
+            else:
+                raise IOError("File not overwritten as per user input.")
+                
+        return out_file_path
+                
+    except IOError as e:
+        raise IOError(f"Could not write to file '{out_file_path}', invalid path.") from e
+
+def json2dict(in_file_path):
+    """
+    Convert a JSON file to a dictionary.
+
+    Parameters 
+    ----------
+    in_file_path : str
+        The input file path, which could be absolute or relative.
+
+    Returns
+    -------
+    dict
+        The content of the JSON file as a dictionary.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the input file is not found.
+    TypeError
+        If the content of the file cannot be decoded as JSON.
+    """
     
+    # Open and read the JSON file #
+    try:
+        with open(in_file_path, 'r') as in_file_obj:
+            content_str = in_file_obj.read()
+    
+        # Convert content to a dictionary #
+        try:
+            content_dict = json.loads(content_str)
+            return content_dict
+        except json.JSONDecodeError as e:
+            raise TypeError(f"Could not decode content from file '{in_file_path}'.") from e
+
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File '{in_file_path}' not found.") from e
 
 # Merge and sort operations #
 #---------------------------#
 
 def sort_dictionary_by_keys(dic):
+    """
+    Sort a dictionary by its keys.
+
+    Parameters
+    ----------
+    dic : dict
+        The dictionary to be sorted.
+
+    Returns
+    -------
+    dict
+        The dictionary sorted by keys.
+    """
     keys_sorted_list = sorted(dic)
-    dic_sorted_byKeys = {key : dic[key]
-                         for key in keys_sorted_list}
-    return dic_sorted_byKeys
-    
+    dic_sorted_by_keys = {key: dic[key] for key in keys_sorted_list}
+    return dic_sorted_by_keys
 
 def merge_dictionaries(dict_list):
-    ldl = len(dict_list)
-    if ldl == 1:
-        raise ValueError("2 dictionaries at least must be passed.")
+    """
+    Merge a list of dictionaries into a single dictionary.
+
+    Parameters
+    ----------
+    dict_list : list
+        The list of dictionaries to be merged.
+
+    Returns
+    -------
+    dict
+        The merged dictionary.
+
+    Raises
+    ------
+    ValueError
+        If the list contains less than two dictionaries.
+    """
+    if len(dict_list) < 2:
+        raise ValueError("At least 2 dictionaries must be passed.")
     
-    str2eval = "{"
+    merged_dict = {}
     for d in dict_list:
-        str2eval += f"**{d},"
-    str2eval += "}"
-    
-    merged_dict = eval(str2eval)
+        merged_dict.update(d)
     return merged_dict
 
-    
 #--------------------------#
 # Parameters and constants #
 #--------------------------#
 
-# File extensions #
+# File extension #
 extension = "json"
