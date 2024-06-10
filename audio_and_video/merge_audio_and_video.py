9984ce633ab8fdfd16b92bc6ffcb351b5aec7417
climatechange-tech <jagabantxo@gmail.com>
2024-06-10 11:43:55 +0200

    Create program that applies method 'merge_audio_and_video_files' in module 'audio_and_video_manipulation.py'

---
+++ audio_and_video/merge_audio_and_video.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+**Note**
+
+This program is an application of the main module 'audio_and_video.py',
+and it uses the 'merge_audio_and_video_files' attributes and/or functions.
+PLEASE DO NOT REDISTRIBUTE this program along any other directory,
+as the module is designed to work with absolute paths.
+"""
+
+#-----------------------#
+# Import custom modules #
+#-----------------------#
+
+from audio_and_video.audio_and_video_manipulation import merge_audio_and_video_files
+
+#-------------------#
+# Define parameters #
+#-------------------#
+
+input_video_file_list = [
+    ]
+
+input_audio_file_list = [
+    ]
+
+# output_file_name_list = None
+output_file_name_list = [
+    ]
+
+ZERO_PADDING = 1
+
+#------------------#
+# Perform the task #
+#------------------#
+
+merge_audio_and_video_files(input_video_file_list,
+                            input_audio_file_list,
+                            output_file_name_list=None,
+                            ZERO_PADDING=ZERO_PADDING)
