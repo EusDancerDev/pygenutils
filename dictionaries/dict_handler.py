7a64b2f2775b291a57b24a131651d1becb27e161
climatechange-tech <jagabantxo@gmail.com>
2024-06-10 22:00:08 +0200

    Update module to manipulate data in dictionaries, and export to or import from JSON files

--- dictionaries/dict_handler.py
+++ dictionaries/dict_handler.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 10 21:52:48 2024
-
-@author: jonander
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 #----------------#
 # Import modules #
