8f7e897e764feb30612dd5f003c3991036b96d34
climatechange-tech <jagabantxo@gmail.com>
2024-06-10 22:42:18 +0200

    Add module for mathematical operations with sets

---
+++ sets_and_intervals/operators_sets.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-----------------------#
+# Import custom modules #
+#-----------------------#
+
+from strings.string_handler import find_substring_index
+
+#-------------------------#
+# Define custom functions #
+#-------------------------#
+
+def operations_with_sets(array_of_sets1, 
+                         array_of_sets2=None, 
+                         obj_type="built-in",
+                         operation="union"):
+
+    arg_names = operations_with_sets.__code__.co_varnames
+    operation_arg_pos = find_substring_index(arg_names, "operation")
+    obj_type_arg_pos = find_substring_index(arg_names, "obj_type")
+    
+    operation_opt_array = list(operation_dict.keys())
+    
+    if operation not in operation_opt_array:
+        raise ValueError(f"Wrong '{arg_names[operation_arg_pos]}' argument. "
+                         f"Options are {operation_opt_array}.")
+        
+        
+    if obj_type not in obj_types:
+        raise ValueError(f"Wrong '{arg_names[obj_type_arg_pos]}' argument. "
+                         f"Options are {obj_types}.")
+        
+    if obj_type == "built-in":
+        if operation == operation_opt_array[-1]:
+            from itertools import product    
+            
+        res_set = eval(operation_dict.get(operation))            
+        return res_set
+            
+    elif obj_type == "sympy":
+        
+        # TODO: garatu 'sympy' motako multzoen kasua
+        # from sympy import FiniteSet
+        raise NotImplementedError("Please for now set argument "
+                                  f"'{arg_names[obj_type_arg_pos]}' to "
+                                  f"{obj_types[0]}.")
+
+#--------------------------#        
+# Parameters and constants #
+#--------------------------#
+
+obj_types = ["built-in", "sympy"]
+
+operation_dict = {
+    'union': "array_of_sets1.union(array_of_sets2)",
+    'intersection': "array_of_sets1.intersection(array_of_sets2)",
+    'difference': "array_of_sets1.difference(array_of_sets2)",
+    'symmetric_difference': "array_of_sets1.symmetric_difference(array_of_sets2)",
+    'cartesian_product': "set(product(array_of_sets1))"
+}
