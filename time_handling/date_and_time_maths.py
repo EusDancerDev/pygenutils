75b9d31888ba6df82ae91609a06cf4b98b213928
climatechange-tech <jagabantxo@gmail.com>
2024-06-12 13:21:44 +0200

    Add docstrings and polish the main methods 'sum_clock_times' and 'sum_date_objects', as well as their auxiliary methods

--- time_handling/date_and_time_maths.py
+++ time_handling/date_and_time_maths.py
@@ -20,17 +20,9 @@ import pandas as pd
 # Import custom modules #
 #-----------------------#
 
-from strings import information_output_formatters, string_handler
+from strings.string_handler import find_substring_index
+from strings.information_output_formatters import format_string, print_format_string
 from time_handling.time_formatters import time2seconds, time_format_tweaker
-
-# Create aliases #
-#----------------#
-
-find_substring_index = string_handler.find_substring_index
-
-format_string = information_output_formatters.format_string
-get_obj_type_str = information_output_formatters.get_obj_type_str
-print_format_string = information_output_formatters.print_format_string
 
 #------------------#
 # Define functions #
@@ -45,89 +37,174 @@ print_format_string = information_output_formatters.print_format_string
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
 # Main method #
-def clock_time_sum(clock_obj_list, operation="sum", 
-                   subtract_start_idx=1,
-                   time_fmt_str="%H:%M:%S"):
-    
+def clock_time_sum(clock_obj_list, operation="sum", str_obj_fmt="%H:%M:%S", output_format="standard"):
+    """
+    Calculate the sum or difference of a list of clock times.
+
+    Parameters
+    ----------
+    clock_obj_list : list, tuple, or numpy.ndarray
+        A list, tuple, or numpy.ndarray containing time objects as strings 
+        that follow the format specified in 'str_obj_fmt'.
+    operation : str, optional
+        The operation to perform on the clock times. Supported operations 
+        are "sum" (default) and "subtr" for subtraction.
+    str_obj_fmt : str, optional
+        The format string that specifies the format of the time objects. 
+        Default is "%H:%M:%S".
+    output_format : {'standard', 'class_object', 'raw'}, optional
+        The output format for the result.
+        Options are "standard" (default) for pandas.Timedelta, 
+        "class_object" for datetime.time objects,
+        and "raw" for a list containing [days, hours, minutes, seconds].
+
+    Returns
+    -------
+    object
+        The total time represented in the specified output format.
+
+    Raises
+    ------
+    TypeError
+        If 'clock_obj_list' is not a list, tuple, or numpy.ndarray, 
+        or if 'output_format' is not a valid option.
+    ValueError
+        If 'clock_obj_list' contains fewer than 2 elements or if an unsupported 
+        operation is specified.
+    """
+
     # Proper date and/or time list format control #
-    arg_names = clock_time_sum.__co_varnames__
-    obj_list_pos = find_substring_index(arg_names, "clock_obj_list")
+    arg_names = clock_time_sum.__code__.co_varnames
+    obj_list_pos = arg_names.index("clock_obj_list")
     
     if isinstance(clock_obj_list, str):
         raise TypeError(f"Argument '{arg_names[obj_list_pos]}' "
                         f"(position {obj_list_pos}) must either be a "
                         "list, tuple or numpy.ndarray.")
     elif (isinstance(clock_obj_list, (list, tuple, np.ndarray)) and len(clock_obj_list) < 2):
-        raise ValueError("At least two date and/or time objects are required "
-                         "to perform the addition.")
+        raise ValueError(f"Argument '{arg_names[obj_list_pos]}' "
+                         "must contain at least two objects.")
     
     # Proper operation argument control #
+    basic_math_opt_list = ["sum", "subtr"]
     if operation not in basic_math_opt_list:
         raise ValueError("Unsupported mathematical operation. "
-                         f"Supported ones are sum and subtractions: {basic_math_opt_list}")
+                         f"Supported ones are: {basic_math_opt_list}")
     
     # Time delta object conversions #
     timedelta_list = []
     for clock_obj in clock_obj_list:
-        time_obj = convert_to_time(clock_obj, time_fmt_str=time_fmt_str)
+        time_obj = convert_to_time(clock_obj, str_obj_fmt=str_obj_fmt)
         timedelta_obj = time_to_timedelta(time_obj)
         timedelta_list.append(timedelta_obj)
         
+    # Perform the arithmetical operations #
+    total_timedelta = operation_dict.get(operation)(timedelta_list)    
+    
+    # Return the result in the specified output format #
+    if output_format == "standard":
+        return total_timedelta
+    elif output_format == "class_object":
+        total_timedelta_time_part = total_timedelta.time()
+        return total_timedelta_time_part
+    elif output_format == "raw":
+        total_timedelta_comp_list = [total_timedelta.days,
+                                     total_timedelta.hour,
+                                     total_timedelta.minute,
+                                     total_timedelta.second]
         
-    # Only for subtractions: time delta list index in range control #
-    len_td_list_idx = len(timedelta_list)-1
-    if (subtract_start_idx < 0 or subtract_start_idx > len_td_list_idx):
-        raise IndexError("List index out of range, it must be between "
-                         f"0 and {len_td_list_idx}.")
-        
-    # Perform the sum or subtract operation # TODO: switch dictionary ???
-    if operation == "sum":
-        total_timedelta = np.sum(timedelta_list)
-    elif operation == "subtract":
-        total_timedelta = \
-        timedelta_list[0] - np.sum(timedelta_list[subtract_start_idx:])        
-    return total_timedelta
-
+        return total_timedelta_comp_list
+   
 
 # Auxiliary methods #
-def convert_to_time(clock_obj, time_fmt_str):
+def convert_to_time(clock_obj, str_obj_fmt):
+    """
+    Convert various representations of time objects to datetime.time.
+
+    Parameters
+    ----------
+    clock_obj : str, numpy.datetime64, time.struct_time, 
+                datetime.datetime, datetime.time, or pandas.Timestamp
+        The input time object to be converted.
+    str_obj_fmt : str
+        The format string that specifies the format of the time objects.
+
+    Returns
+    -------
+    datetime.time
+        The converted time object.
+        
+    Raises
+    ------
+    TypeError
+        If the input object type is not supported.
+    """
     if isinstance(clock_obj, str):
-        datetime_obj = time_format_tweaker(clock_obj, time_fmt_str=time_fmt_str)
-        time_obj = datetime_obj.time()
+        datetime_obj = time_format_tweaker(clock_obj, str_obj_fmt=str_obj_fmt)
+        time_obj = return_time_part(datetime_obj)
         return time_obj
     elif isinstance(clock_obj, np.datetime64):
         datetime_obj = time_format_tweaker(clock_obj, method="datetime_tolist")
-        time_obj = datetime_obj.time()
+        time_obj = return_time_part(datetime_obj)
         return time_obj
     elif isinstance(clock_obj, _time.struct_time):
-        time_obj = datetime.time(clock_obj.tm_hour, clock_obj.tm_min, clock_obj.tm_sec)
+        datetime_obj = clock_obj
+        dt_parts_list = [clock_obj.tm_hour, clock_obj.tm_min, clock_obj.tm_sec]
+        time_obj = return_time_part(datetime_obj, dt_parts_list)
         return time_obj
     elif isinstance(clock_obj, datetime.datetime):
-        time_obj = clock_obj.time()
+        time_obj = return_time_part(clock_obj)
         return time_obj
     elif isinstance(clock_obj, datetime.time):
-        time_obj = clock_obj
-        return time_obj
+        return clock_obj
     elif isinstance(clock_obj, pd.Timestamp):
-        time_obj = clock_obj.time()
-        return time_obj
+        time_obj = return_time_part(clock_obj)
+        return time_obj    
     else:
-        raise TypeError("Unsupported type")
+        time_conv_arg_list = ["time or datetime", "time"]
+        raise TypeError(format_string(unsupported_obj_type_str, time_conv_arg_list))
         
+        
+def return_time_part(datetime_obj, arg_list=None):
+    """
+    Return the time part of a datetime object.
+
+    Parameters
+    ----------
+    datetime_obj : datetime.datetime
+        The datetime object from which to extract the time part.
+    arg_list : list, optional
+        List containing specific parts of the datetime object.
+
+    Returns
+    -------
+    datetime.time
+        The time part of the datetime object.
+    """
+    if arg_list is None:
+        time_obj = datetime_obj.time()
+    else:
+        time_obj = datetime_obj.time(*arg_list)
+    return time_obj
 
 def time_to_timedelta(t):
+    """
+    Convert a time object to a timedelta object.
+
+    Parameters
+    ----------
+    t : datetime.time
+        The time object to be converted.
+
+    Returns
+    -------
+    pd.Timedelta
+        The time represented as a timedelta object.
+    """
     timedelta_obj = pd.Timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
     return timedelta_obj
 
 
-time_list = [
-    '2024-03-12 12:30:45',
-    np.datetime64('2024-03-12T08:45:30'),
-    _time.localtime(_time.mktime((2024, 3, 12, 15, 30, 0, 0, 0, -1))),
-    datetime.datetime(2024, 3, 12, 14, 45, 15),
-    datetime.time(9, 30, 0),
-    pd.Timestamp('2024-03-12 07:20:30')
-]
 
 # Time average #
 #-#-#-#-#-#-#-#-
@@ -212,39 +289,42 @@ def radians_to_time_of_day(x):
 # Sum and subtract operations #
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 
-def sum_date_objects(date_list):
-    # if len(date_list) < 2:
-    #     raise ValueError("At least two dates are required to perform the addition.")
+def sum_date_objects(date_list, operation="sum", str_obj_fmt="%Y-%m-%d"):
+
+    """
+    Parameters
+    ----------
+    str_obj_fmt : str
+        String that identifies the format of those datetime objects of type string.
+    """
     
     # Proper date and/or time list format control #
-    arg_names = sum_date_objects.__co_varnames__
+    arg_names = sum_date_objects.__code__.co_varnames
     date_list_pos = find_substring_index(arg_names, "date_list")
-
+    
     if isinstance(date_list, str):
         raise TypeError(f"Argument '{arg_names[date_list_pos]}' "
                         f"(position {date_list_pos}) must either be a "
                         "list, tuple or numpy.ndarray.")
     elif (isinstance(date_list, (list, tuple, np.ndarray)) and len(date_list) < 2):
-        raise ValueError("At least two date and/or time objects are required "
-                         "to perform the addition.")
-
+        raise ValueError(format_string(too_few_arg_error_str, "time"))
+    
     # Proper operation argument control #
-    # if operation not in basic_math_opt_list:
-    #     raise ValueError("Unsupported mathematical operation. "
-    #                      f"Supported ones are sum and subtractions: {basic_math_opt_list}")
-    
-    # Date or datetime object conversions to date only #
-    total_date = convert_to_date(date_list[0])
+    if operation not in basic_math_opt_list:
+        raise ValueError("Unsupported mathematical operation. "
+                         f"Supported ones are sum and subtractions: {basic_math_opt_list}")
+   
+    # Perform the aritmethical operations #
+    total_date = convert_to_date(date_list[0], str_obj_fmt=str_obj_fmt)
     for obj in date_list[1:]:
-        date_obj = convert_to_date(obj)
-        total_date = add_dates_with_year_gap(total_date, date_obj)
-    
+        date_obj = convert_to_date(obj, str_obj_fmt=str_obj_fmt)
+        total_date = add_dates_with_year_gap(total_date, date_obj, operation=operation)
     return total_date
 
 
-def convert_to_date(obj, time_fmt_str="%Y-%m-%d"):
+def convert_to_date(obj, str_obj_fmt):
     if isinstance(obj, str):
-        datetime_obj = datetime.datetime.strptime(obj, time_fmt_str)
+        datetime_obj = datetime.datetime.strptime(obj, str_obj_fmt)
         date_obj = datetime_obj.date()
         return date_obj
     elif isinstance(obj, np.datetime64):
@@ -260,47 +340,61 @@ def convert_to_date(obj, time_fmt_str="%Y-%m-%d"):
         date_obj = obj.date()
         return date_obj
     else:
-        raise TypeError("Unsupported type")
+        date_conv_arg_list = ["date or datetime", "date"]
+        raise TypeError(format_string(unsupported_obj_type_str, date_conv_arg_list))
+        
 
-def add_dates_with_year_gap(date1, date2):
+def add_dates_with_year_gap(date1, date2, operation):
+    
     # Extract year, month, and day from both dates
     year1, month1, day1 = date1.year, date1.month, date1.day
     year2, month2, day2 = date2.year, date2.month, date2.day
 
     # Calculate the gap in years
     year_gap = abs(year2 - year1)
-    
-    # Add the months and days
-    new_month = month1 + month2
-    new_day = day1 + day2
-    
-    # Adjust the month and day for overflow
-    while new_month > 12:
-        new_month -= 12
-        year_gap += 1
 
-    # Create a dummy date to handle day overflow
-    while True:
-        try:
-            result_date = datetime.date(year1 + year_gap, new_month, new_day)
-            break
-        except ValueError:
-            new_day -= 1
+    # Date additions #
+    ##################
+    
+    if operation == "sum":        
+        # Add the months and days
+        new_month = month1 + month2
+        new_day = day1 + day2
+        
+        # Adjust the month and day for overflow
+        while new_month > 12:
+            new_month -= 12
+            year_gap += 1
+    
+        # Create a dummy date to handle day overflow
+        while True:
+            try:
+                result_date = datetime.date(year1 + year_gap, new_month, new_day)
+                break
+            except ValueError:
+                new_day -= 1
+                
+                
+    elif operation == "subtr":
+        # Subtract the months and days
+        new_month = month1 - month2
+        new_day = day1 - day2
+        
+        # Adjust the month and day for underflow
+        while new_month < 1:
+            new_month += 12
+            year_gap += 1
+    
+        # Create a dummy date to handle day underflow
+        while True:
+            try:
+                result_date = datetime.date(year1 - year_gap, new_month, new_day)
+                break
+            except ValueError:
+                new_day += 1
 
     return result_date
 
-
-# Example usage
-date_list = [
-    '2024-03-12',
-    np.datetime64('2025-12-12'),
-    datetime.datetime(2026, 5, 10),
-    datetime.date(2027, 7, 8),
-    pd.Timestamp('2028-09-15')
-]
-
-result = sum_date_objects(date_list)
-print("Resulting date:", result)
 
 # Natural years #
 #-#-#-#-#-#-#-#-#
@@ -400,4 +494,23 @@ basic_math_opt_list = ["sum", "subtr"]
 #----------------------#
 
 # Error strings #
-type_error_str = """Argument '{}' at position {} must be of type '{}'."""
+type_error_str = "Argument '{}' at position {} must be of type '{}'."
+        
+unsupported_obj_type_str = """Unsupported {} type. Supported types are:
+    - string
+    - datetime.datetime
+    - datetime.{}
+    - numpy.datetime64
+    - pandas.Timestamp
+"""
+
+too_few_arg_error_str = \
+"At least two {} or datetime objects are required to perform the addition."
+
+# Switch case dictionaries #
+#--------------------------#
+
+operation_dict = {
+    "sum": np.sum,
+    "subtr": lambda tds: tds[0] - np.sum(tds[1:])
+}
