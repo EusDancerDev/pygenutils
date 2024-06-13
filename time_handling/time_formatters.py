74435971d2ee6c08b5ac0a43dd005a3b490a783d
climatechange-tech <jagabantxo@gmail.com>
2024-06-13 16:01:24 +0200

    Separate mechanisms for pandas.DataFrame and pandas.Series objects, regarding result object formatting

--- time_handling/time_formatters.py
+++ time_handling/time_formatters.py
@@ -360,6 +360,7 @@ def time_format_tweaker(t,
         if isinstance(t_res, pd.DataFrame):
             for col in t_res.columns:
                 t_res.loc[:, col] = t_res.loc[:, col].dt.strftime(time_fmt_str)                
+            return t_res
         elif isinstance(t_res, np.ndarray):
             t_res = t_res.astype('U')
             return t_res
