523ae7bfe4770e2f90c6d9267a05fc2d0e12c29e
climatechange-tech <jagabantxo@gmail.com>
2024-06-18 00:03:29 +0200

    Add method to retrieve the name of a method defined in situ

--- strings/information_output_formatters.py
+++ strings/information_output_formatters.py
@@ -74,19 +74,45 @@ def print_percent_string(string2format, arg_obj):
     except SyntaxError:
         raise SyntaxError(syntax_error_str)
         
+        
+# Object information gatherers #
+#------------------------------#
 
 # Object type's class to string conversions #
-#-------------------------------------------#
-
 def get_obj_type_str(obj):
     obj_type_class = type(obj)
     obj_type_str = obj_type_class.__name__
     return obj_type_str
 
+# Retrieve a function's name to use for visualization purposes #
+def retrieve_function_name(library="inspect"):
+    """Returns the name of the method that called this function."""
+
+    if library not in method_name_retrieval_libraries:
+        raise ValueError("Unsupported library, choose one from "
+                         f"{method_name_retrieval_libraries}.")
+    else:
+        if library == "inspect":
+            import inspect
+            current_frame = inspect.currentframe()
+            caller_frame = current_frame.f_back  # Get the caller's frame
+            frame_info = inspect.getframeinfo(caller_frame)
+            return frame_info.function
+        elif library == "sys":
+            import sys
+            """Returns the name of the method that called this function using sys._getframe()."""
+            method_name = sys._getframe(1).f_code.co_name  # Get the caller's frame (1 level up)
+        return method_name
+
+
 #--------------------------#
 # Parameters and constants #
 #--------------------------#
 
+# Supported library list for method name retrievals #
+method_name_retrieval_libraries = ["inspect", "sys"]
+
+# Error strings #
 type_error_str1 = "Check the iterable type passed to the instance."
 type_error_str2 = "Argument must be of type 'str' only."
 
