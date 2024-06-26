2df3df6d6ce7c66ad4be7d8a2038b5e991f02c7f
climatechange-tech <jagabantxo@gmail.com>
2024-06-26 12:42:24 +0200

    Create subpackage for standalone programs

---
+++ utilities/scripts/bulk_rename_index_exec.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+**Note**
+
+This program is an application of the main module 'bulk_rename_index_main',
+and it uses some of its attributes and/or functions.
+YOU MAY REDISTRIBUTE this program along any other directory,
+but keep in mind that the module is designed to work with absolute paths.
+"""
+
+#-----------------------#
+# Import custom modules #
+#-----------------------#
+
+from pytools.files_and_directories.bulk_rename_index_main import reorder_objs
+
+#-------------------#
+# Define parameters #
+#-------------------#
+
+path = "/home/jonander/Pictures/2023/Tenerife_test_rename_pytools"
+obj_type = "file"
+
+ZERO_PADDING = 3
+extensions2skip = ""
+
+starting_number = "default"
+index_range = "all"
+
+splitdelim = None
+
+#------------------#
+# Perform the task #
+#------------------#
+
+reorder_objs(path,
+             obj_type,
+             extensions2skip,
+             index_range,
+             starting_number,
+             ZERO_PADDING,
+             splitdelim=splitdelim)
