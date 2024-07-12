58eac67dc160da73c8bbef1edd8e3f56e7ba5d10
climatechange-tech <jagabantxo@gmail.com>
2024-07-12 09:15:28 +0200

    Add to-do list to incorporate 'os' and 'subprocess' modules' methods to the functions

--- operative_systems/os_operations.py
+++ operative_systems/os_operations.py
@@ -7,11 +7,19 @@
 
 import os
 
+#-----------------------#
+# Import custom modules # 
+#-----------------------#
+
+from pyutils.parameters_and_constants.global_parameters import filesystem_context_modules
+
 #------------------#
 # Define functions #
 #------------------#
 
 def exec_shell_command(command_str):
+    # TODO: eman aukera 'os' edota 'subprocess' moduluak erabiltzeko
+    # TODO: aurrekoa kontuan hartuz, horietako bat ere aukeratzen ez bada, errorea jaulki
     """
     Execute a shell command.
 
@@ -27,6 +35,8 @@ def exec_shell_command(command_str):
     os.system(command_str)
     
 def catch_shell_prompt_output(command_str):
+    # TODO: eman aukera 'os' edota 'subprocess' moduluak erabiltzeko
+    # TODO: aurrekoa kontuan hartuz, horietako bat ere aukeratzen ez bada, errorea jaulki
     """
     Execute a shell command and capture its output.
 
@@ -42,3 +52,10 @@ def catch_shell_prompt_output(command_str):
     """
     output_str = os.popen(command_str).read()
     return output_str
+
+
+#--------------------------#
+# Parameters and constants #
+#--------------------------#
+
+native_os_operation_modules = filesystem_context_modules[0::3]
