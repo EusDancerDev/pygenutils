f6c2e6cf2730ae59dda516ceb15911553c8a946f
climatechange-tech <jagabantxo@gmail.com>
2024-09-06 20:14:01 +0200

    Delete file 'webdriver_Firefox-Chrome_settings_check.py' in favour of the more descriptive and precise 'webdriver_Firefox-Chrome_setup_test.py' file name; the code remains the same and tested, it works properly.

--- web_scraping/webdriver_Firefox-Chrome_setup_test.py
+++ web_scraping/webdriver_Firefox-Chrome_setup_test.py
@@ -7,6 +7,7 @@ Created on Sat Aug 17 10:53:39 2024
 """
 
 from selenium import webdriver
+# import sys
 
 options = webdriver.FirefoxOptions()
 options.add_argument('--headless')
@@ -14,6 +15,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 
 # Specify the path to the GeckoDriver executable
-# gecko_driver_path = '/path/to/geckodriver'  # Update with the actual path to geckodriver
+# sys.path.insert(0,'/usr/local/bin/geckodriver')
 
 wd = webdriver.Firefox(options=options)
+wd.quit()
