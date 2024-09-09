8c9ce524818d1fcfad6883776afb539f64f03ba5
climatechange-tech <jagabantxo@gmail.com>
2024-09-09 15:41:33 +0200

    Improve section header comments of method 'merge_audio_and_video_files'; plan implementation of subprocess.run class to catch ffmpeg-related errors and switch syntaxes

--- audio_and_video/audio_and_video_manipulation.py
+++ audio_and_video/audio_and_video_manipulation.py
@@ -30,7 +30,10 @@ def merge_audio_and_video_files(input_video_file_list,
                                 output_file_name_list=None,
                                 ZERO_PADDING=1):
     
-    # Quality control of the zero-padding #
+    # Input validations #
+    #-------------------#
+    
+    # Zero-padding #
     all_arg_names = get_caller_method_args()
     zp_arg_pos = find_substring_index(all_arg_names, "ZERO_PADDING")
     
@@ -44,44 +47,50 @@ def merge_audio_and_video_files(input_video_file_list,
                         "Set to `None` if no zero padding is desired.")
         
         
+    
     # Check whether the compulsory lists are of the same length #
     livfl = len(input_video_file_list)
     liafl = len(input_audio_file_list)
     
     if livfl != liafl:
-        raise ValueError("Input audio and video files have to be of the same length.")
+        raise ValueError("Input audio and video file lists have to be of the same length.")
+    
+    # Operations #
+    #------------#       
+    
+    # If output file name list is not provided, create a default one #
+    if output_file_name_list is None:
+        if ZERO_PADDING is None:
+            output_file_name_list_default = [f"merged_video_{i:d}"
+                                             for i in range(1, livfl+1)]
+        else:
+            output_file_name_list_default = [f"merged_video_{i:0{ZERO_PADDING+1}d}"
+                                             for i in range(1, livfl+1)]
+        lofnl = len(output_file_name_list_default)
     else:
-        # If output file name list is not provided, create a default one #
-        if output_file_name_list is None:
-            if ZERO_PADDING is None:
-                output_file_name_list_default = [f"merged_video_{i:d}"
-                                                 for i in range(1, livfl+1)]
-            else:
-                output_file_name_list_default = [f"merged_video_{i:0{ZERO_PADDING+1}d}"
-                                                 for i in range(1, livfl+1)]
-            lofnl = len(output_file_name_list_default)
-        else:
-            lofnl = len(output_file_name_list)
-            
-        length_lists = [livfl, liafl, lofnl]
-        unique_lengths = np.unique(length_lists)
-        lul = len(unique_lengths)
+        lofnl = len(output_file_name_list)
         
-        if lul > 1:
-            raise ValueError("All lists have to be of the same length.")
-            
-        # Perform the operations #
-        else:
-            for in_audio_fn, in_video_fn, out_video_fn in zip(input_audio_file_list,
-                                                              input_video_file_list,
-                                                              output_file_name_list):
+    unique_lengths = np.unique([livfl, liafl, lofnl])
+    lul = len(unique_lengths)
+    
+    # Previous equal length check with the output file list (default or user-defined) #
+    if lul > 1:
+        raise ValueError("Input audio and video file lists are of the same length, "
+                         "but the output file lists length mismatches it.")
+    
+    # Merge the provided audio and video files #
+    for in_audio_fn, in_video_fn, out_video_fn in zip(input_audio_file_list,
+                                                      input_video_file_list,
+                                                      output_file_name_list):
+        
+        arg_tuple = (in_audio_fn, in_video_fn, out_video_fn)
+        merge_command = format_string(ffmpeg_aud_vid_merge_syntax, arg_tuple)
+        os.system(merge_command) # FIXME: erabil subprocess.run
                 
-                arg_tuple = (in_audio_fn, in_video_fn, out_video_fn)
-                merge_command = format_string(ffmpeg_aud_vid_merge_syntax, arg_tuple)
-                os.system(merge_command)
                 
-                
-# TODO: bi bideo artxibo alkartzeko metodoa eraiki
+# TODO: hainbat bideo artxibo alkartzeko metodoa eraiki
+def merge_video_files(video_file_list):
+    pass
 
 #--------------------------#
 # Parameters and constants #
@@ -91,7 +100,5 @@ def merge_audio_and_video_files(input_video_file_list,
 #----------------------#
 
 # FFMPEG merge syntaxes #
-ffmpeg_aud_vid_merge_syntax = "ffmpeg -i {} -i {} -c:v copy -c:a aac {}"
+ffmpeg_aud_vid_merge_syntax = "ffmpeg -i {} -i {} -c:v copy -c:a aac {}" # Possible error: DTS Non-monotonous error -> catch with subprocess.run
 ffmpeg_vid_merge_syntax = "ffmpeg -i 'concat:{}|{}' -c copy {}"
-
-
