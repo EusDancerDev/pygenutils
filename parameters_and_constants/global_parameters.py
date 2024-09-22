728c36eee7edccc6a2032f38501f370f6a159442
climatechange-tech <jagabantxo@gmail.com>
2024-09-22 12:29:55 +0200

    Refine and reorganize module for scalability
    Group everything under broader categories (Time, Mathematical, Programming, and Socio-Economical). Adjust dictionary keys to use uppercase to align with best practices. Clear the structure and make sections easily organisable and modifiable as new parameters arise. Split out time formats from non-standard and custom formats for clarity.

--- parameters_and_constants/global_parameters.py
+++ parameters_and_constants/global_parameters.py
@@ -1,142 +1,126 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 Created on Thu Jun  9 12:14:15 2022
 
 @author: jon ander
 
 ** DISCLAIMER **
-This program serves as a module to store whatever type of parameters
-that are considered as frequent use.
-
-The parameters are organized similar as are
-the directories inside "pytools" directory.
+This program serves as a module to store parameters that are used frequently or globally.
 """
 
-#%% PHYSICAL CONCEPTS
+#%% GLOBAL PARAMETERS MODULE STRUCTURE
+
+# 1. Time-Related Parameters
+# 2. Mathematical Concepts
+# 3. Programming Concepts
+# 4. Socio-Economical Concepts
+
+#%% 1. TIME-RELATED PARAMETERS
 
 #------#
 # Time #
 #------#
 
-basic_time_format_strs = dict(
-    H           = "%Y-%m-%d %H:%M:%S",
-    H_NODATESEP = "%Y%m%d %H:%M:%S",
-    D           = "%Y-%m-%d",
-    D_NODATESEP = "%Y%m%d",
-    M           = "%Y-%m",
-    Y           = "%Y"
-)
+# Basic time format strings
+basic_time_format_strs = {
+    "H": "%Y-%m-%d %H:%M:%S",
+    "H_NODATESEP": "%Y%m%d %H:%M:%S",
+    "D": "%Y-%m-%d",
+    "D_NODATESEP": "%Y%m%d",
+    "M": "%Y-%m",
+    "Y": "%Y"
+}
 
-non_std_time_format_strs = dict(
-    CFT_H = "%a %b %d %H:%M:%S %Y",
-    CFT_D = "%a %b %d %Y",
-    CFT_M = "%b %Y"
-)
+# Non-standard time format strings
+non_std_time_format_strs = {
+    "CFT_H": "%a %b %d %H:%M:%S %Y",
+    "CFT_D": "%a %b %d %Y",
+    "CFT_M": "%b %Y"
+}
 
-# TODO: gehitu beste batzuk
+# Custom time format strings
+custom_time_format_strs = {
+    "CT_EXCEL_SPANISH_H": "%d/%m/%y %H:%M:%S",
+    "CT_EXCEL_SPANISH_NOBAR_H": "%d%m%y %H:%M:%S",
+    "CT_EXCEL_SPANISH_D": "%d/%m/%y",
+    "CT_EXCEL_SPANISH_NOBAR_D": "%d%m%y"
+}
 
-custom_time_format_strs = dict(
-    CT_EXCEL_SPANISH_H       = "%d/%m/%y %H:%M:%S",
-    CT_EXCEL_SPANISH_NOBAR_H = "%d%m%y %H:%M:%S",
-    CT_EXCEL_SPANISH_D       = "%d/%m/%y",
-    CT_EXCEL_SPANISH_NOBAR_D = "%d%m%y"
-)
-
+# Month number to letter mapping
 month_number_dict = {
-    1  : "J",
-    2  : "F",
-    3  : "M",
-    4  : "A",
-    5  : "M",
-    6  : "J",
-    7  : "J",
-    8  : "A",
-    9  : "S",
-    10 : "O",
-    11 : "N",
-    12 : "D"
+    1: "J", 2: "F", 3: "M", 4: "A", 5: "M", 6: "J", 7: "J", 
+    8: "A", 9: "S", 10: "O", 11: "N", 12: "D"
 }
 
+# Seasonal time frequency dictionary
 season_time_freq_dict = {
-    1  : "Q-JAN",
-    2  : "Q-FEB",
-    3  : "Q-MAR",
-    4  : "Q-APR",
-    5  : "Q-MAY",
-    6  : "Q-JUN",
-    7  : "Q-JUL",
-    8  : "Q-AUG",
-    9  : "Q-SEP",
-    10 : "Q-OCT",
-    11 : "Q-NOV",
-    12 : "Q-DEC"
+    1: "Q-JAN", 2: "Q-FEB", 3: "Q-MAR", 4: "Q-APR", 
+    5: "Q-MAY", 6: "Q-JUN", 7: "Q-JUL", 8: "Q-AUG", 
+    9: "Q-SEP", 10: "Q-OCT", 11: "Q-NOV", 12: "Q-DEC"
 }
 
+# Mathematical approximation for year length
 MATHEMATICAL_YEAR_DAYS = 360
 
+
+# Time frequencies
 time_frequencies_complete = ["year", "season", "month", "day", "hour", "minute", "second"]
 time_frequencies_short_1 = ["yearly", "seasonal", "monthly", "daily", "hourly"]
 time_frequencies_shorter_1 = ["year", "seas", "mon", "day", "hour"]
 
-# Supported Pandas date units #
+# Supported date units
 pandas_date_unit_list = ['D', 'ms', 'ns', 's', 'us']
-# Supported Numpy date units #
 numpy_date_unit_list = ['Y', 'M', 'D', 'h', 'm', 's', 'ms', 'us', 'ns']
 
 unit_factor_dict = {
-    "D"  : 1000,
-    "s"  : 1,
-    "ms" : 1e-3,
-    "us" : 1e-6,
-    "ns" : 1e-9
+    "D": 1000,
+    "s": 1,
+    "ms": 1e-3,
+    "us": 1e-6,
+    "ns": 1e-9
 }
 
-#%% MATHEMATICAL CONCEPTS
- 
-#-----------------# 
-# Basic operators #
-#-----------------# 
+#%% 2. MATHEMATICAL CONCEPTS
 
+# Basic operators
 basic_four_rules = ["+", "-", "*", "/"]
 
-#-------------# 
-# Set algebra #
-#-------------# 
+# Set algebra
+operations_sets_list = [
+    "union", "difference", "intersection", 
+    "symmetric_difference", "comparison"
+]
 
-operations_sets_list = ["union", "difference", "intersection",
-                        "symmetric_difference", "comparison"]
+#%% 3. PROGRAMMING CONCEPTS
 
-#%% PROGRAMMING CONCEPTS
-
-#-------------------# 
-# Operative Systems #
-#-------------------# 
-
-# All filesystem operation related modules #
-filesystem_context_modules = ["os", "Path", "shutil", "subprocess"] # 'Path' from 'pathlib' module
-
-# Storage entities #
+# Operative Systems
+filesystem_context_modules = ["os", "Path", "shutil", "subprocess"]  # 'Path' from 'pathlib' module
 storage_entity_types = ["file", "directory"]
 
-#---------------------------------# 
-# Widely used regular expressions #
-#---------------------------------# 
-
+# Regular expressions
 regex_passwords = r"^(?=.{8,})(?=.*[a-z\s])(?=.*[A-Z\s])(?=.*\d)(?=.*[_\W]).+$"
 
-#---------# 
-# Strings #
-#---------#
-
+# Strings
 common_delim_list = ["_", "-", ";", ",", "\n", "\t", " "]
 
+# Databases
+config_dict = {
+    "username": "username",
+    "password": "cool-password",
+    "host": "host",
+    "database_name": "dbname"
+}
 
-#%% SOCIO-ECONOMICAL CONCEPTS
+data_uploading_error_dict = {
+    "1007": "Database already exists",
+    "1045": "Wrong username",
+    "1049": "Unknown database name",
+    "1698": "Wrong password",
+    "2003": "Wrong host name"
+}
 
-#----------------# 
-# Climate change #
-#----------------# 
+#%% 4. SOCIO-ECONOMICAL CONCEPTS
 
+# Climate change
 emission_rcp_scenarios = ["historical", "rcp26", "rcp45", "rcp85"]
