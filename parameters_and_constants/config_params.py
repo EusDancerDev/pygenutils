27a054f547795d6d37e86d8e9ca94d507855f19a
climatechange-tech <jagabantxo@gmail.com>
2024-09-22 12:42:56 +0200

    Add module to store configuration data like credentials, host info, etc.

---
+++ parameters_and_constants/config_params.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 12:35:48 2024
+
+@author: jonander
+
+** DISCLAIMER **
+This program serves as a module to store configuration data
+like credentials, host info, etc.,
+
+*GLOBAL PARAMETERS MODULE STRUCTURE*
+
+1. Programming Concepts
+"""
+
+#%% 1. PROGRAMMING CONCEPTS
+
+# Databases
+config_dict = {
+    "username": "username",
+    "password": "cool-password",
+    "host": "host",
+    "database_name": "dbname"
+}
