e48e3c1b85bd3904438832c5dc64ea8bd1e635f3
climatechange-tech <jagabantxo@gmail.com>
2024-09-23 12:43:14 +0200

    Refactor command execution module
    Enhanced  to utilize helper functions for better structure and readability.
    - Added helper functions: , , , , and  to handle command execution across different modules and classes.
    - Implemented error handling and output capturing in helper functions, supporting pipelines for commands passed to .
    - Created  method to provide readable output for process exit statuses, including error handling for command string interpretation.
    - Improved docstrings across methods for clarity on parameters, returns, and exceptions.
    - Updated command helpers dictionary to include all relevant helper functions.
    This refactor aims to improve scalability, maintainability, and usability of the command execution process.

--- operative_systems/os_operations.py
+++ operative_systems/os_operations.py
@@ -12,50 +12,368 @@ import os
 #-----------------------#
 
 from pyutils.parameters_and_constants.global_parameters import filesystem_context_modules
+from pyutils.strings.string_handler import get_obj_type_str
+from pyutils.strings.information_output_formatters import print_format_string
 
 #------------------#
 # Define functions #
 #------------------#
 
-def exec_shell_command(command_str):
-    # TODO: eman aukera 'os' edota 'subprocess' moduluak erabiltzeko
-    # TODO: aurrekoa kontuan hartuz, horietako bat ere aukeratzen ez bada, errorea jaulki
+# Main method #
+#-------------#
+
+def run_system_command(command,
+                       module="subprocess", 
+                       _class="run",
+                       capture_output=False,
+                       return_output_name=False,
+                       encoding=None):
+   
     """
-    Execute a shell command.
+    Execute a system command using the specified module and class combination.
+
+    This method runs a command using either the 'os' or 'subprocess' module,
+    depending on the provided parameters. It returns relevant output attributes
+    like stdout, stderr, stdin, and the return code, depending on the system 
+    command execution method.
 
     Parameters
     ----------
-    command_str : str
-        The shell command to execute.
-    
+    command : str or list
+        The command to execute, either as a string (for 'os' and 'subprocess')
+        or a list of arguments (only for 'subprocess' module).
+    module : str, optional, default: "subprocess"
+        The module to use for executing the command.
+        Valid options are 'os' or 'subprocess'.
+    _class : str, optional, default "run"
+        The class within the module to use.
+        Valid options are:
+        - 'os': {'system', 'popen'}.
+        - 'subprocess': {'Popen', 'call', 'run'}.
+    capture_output : bool, optional, default: False
+        If True, captures the command's stdout and stderr.
+    return_output_name : bool, optional, default: False
+        If True, returns the file descriptors' names (if applicable) for stdin,
+        stdout, and stderr.
+    encoding : str, optional, default None
+        The encoding to use when decoding stdout and stderr. 
+        If None, no decoding is applied.
+        
+    Raises
+    ------
+    ValueError
+        If the combo (module, _class) is neither of the allowed ones in 'command_helpers' 
+
     Returns
     -------
-    None
+    result : dict
+        A dictionary containing relevant output characteristics such as:
+        - 'stdout': Captured standard output (if applicable)
+        - 'stderr': Captured standard error (if applicable)
+        - 'stdin': The name of the input stream (if applicable)
+        - 'return_code': The exit code of the command
+        - 'errors': Any errors encountered during command execution (if applicable)
     """
-    os.system(command_str)
+
     
-def catch_shell_prompt_output(command_str):
-    # TODO: eman aukera 'os' edota 'subprocess' moduluak erabiltzeko
-    # TODO: aurrekoa kontuan hartuz, horietako bat ere aukeratzen ez bada, errorea jaulki
+    # Validate module and class
+    if (module, _class) not in command_helpers:
+        raise ValueError(f"Unsupported module-class combo '{module}'-'{_class}'.")
+    
+    # Get the appropriate helper function
+    helper_func = command_helpers.get((module, _class))
+    
+    # Run the command via the helper
+    result = helper_func(command, capture_output=capture_output, encoding=encoding)
+    
+    return result
+
+
+# Helpers #
+#---------#
+
+def os_system_helper(command, capture_output):
     """
-    Execute a shell command and capture its output.
+    Helper function to execute a command using os.system.
 
     Parameters
     ----------
-    command_str : str
-        The shell command to execute.
+    command : str
+        The system command to execute.
+    capture_output : bool, optional
+        Cannot capture output with os.system. This argument raises a ValueError 
+        if set to True.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing:
+        - 'return_code': The exit code of the command.
+    """
     
+    # Validations #
+    #-------------#
+    
+    # Command and class #
+    if not isinstance(command, str):
+        obj_type = get_obj_type_str(command)
+        raise TypeError(f"Expected str, not '{obj_type}'.")
+     
+    # Output capturing #
+    if capture_output:
+        raise ValueError("os.system cannot capture output.")
+    
+    # Operations #
+    #------------#
+    
+    # Execute the command
+    exit_code = os.system(command)
+    
+    # Return the exit status
+    return dict(return_code=exit_code)
+
+
+def os_popen_helper(command, capture_output):
+    """
+    Helper function to execute a command using os.popen.
+
+    Parameters
+    ----------
+    command : str
+        The system command to execute.
+    capture_output : bool, optional
+        Must be True for os.popen to capture output.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing:
+        - 'stdout': The captured standard output.
+        - 'return_code': None, as os.popen does not provide a return code.
+    """
+    
+    # Validations #
+    #-------------#
+    
+    # Command and class #
+    if not isinstance(command, str):
+        obj_type = get_obj_type_str(command)
+        raise TypeError(f"Expected str, not '{obj_type}'.")
+        
+    # Output capturing #
+    if not capture_output:
+        raise ValueError("os.popen must capture output.")
+    
+    # Operations #
+    #------------#
+    
+    # Capture the output
+    output = os.popen(command).read()
+    
+    # No return code is available for os.popen, return the output
+    return dict(stdout=output, return_code=None)
+
+
+def subprocess_popen_helper(command, capture_output, encoding=None):
+    """
+    Helper function to execute a command using subprocess.Popen.
+
+    Parameters
+    ----------
+    command : list or str
+        The system command to execute.
+    capture_output : bool, optional
+        If True, captures stdout, stderr, and stdin.
+    encoding : str, optional, default: None
+        The encoding to use when decoding stdout and stderr.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing:
+        - 'stdin': Captured standard input (if applicable).
+        - 'stdout': The captured standard output (if applicable).
+        - 'stderr': The captured standard error (if applicable).
+        - 'return_code': The exit code of the command.
+        - 'errors': Any errors encountered during command execution.
+    """
+    from subprocess import Popen, PIPE
+    
+    # Define the I/O streams
+    pipe_kwargs = dict(stdin=PIPE, stdout=PIPE, stderr=PIPE) if capture_output else {}
+    
+    # Execute the command
+    process = Popen(command, **pipe_kwargs)
+    
+    # Wait for command to complete
+    process.wait()
+    
+    # Capture stdin, stdout, stderr and command exit status errors if requested
+    stdin = process.stdin.name if (capture_output and process.stdin) else None
+    stdout = process.stdout.read().decode(encoding) if capture_output and process.stdout else None
+    stderr = process.stderr.read().decode(encoding) if capture_output and process.stderr else None
+    errors = process.errors if hasattr(process, "errors") else None
+    
+    # Return relevant data
+    return dict(stdin=stdin,
+                stdout=stdout,
+                stderr=stderr,
+                return_code=process.returncode,
+                errors=errors)
+
+
+def subprocess_call_helper(command, capture_output):
+    """
+    Helper function to run a command using subprocess.call.
+    
+    Parameters
+    ----------
+    command : str or list
+        The command to run, either as a string or a list of strings.
+    capture_output : bool
+        If True, output capturing is requested (not supported by this method).
+
+    Raises
+    ------
+    ValueError
+        If capture_output is set to True, since subprocess.call does not support capturing output.
+
+    Returns:
+    --------
+    dict
+        A dictionary containing the return code of the command execution.
+    """
+    from subprocess import call
+    
+    # Validate capture_output (not applicable for subprocess.call)
+    if capture_output:
+        raise ValueError("subprocess.call does not support capturing output.")
+    
+    # Execute the command
+    return_code = call(command)
+    
+    # Return the return code
+    return dict(return_code=return_code)
+
+
+def subprocess_run_helper(command, capture_output, encoding=None):
+    """
+    Helper function to execute a command using subprocess.run.
+
+    Parameters
+    ----------
+    command : list or str
+        The system command to execute.
+    capture_output : bool, optional
+        If True, captures stdout and stderr.
+    encoding : str, optional, default: None
+        The encoding to use when decoding stdout and stderr.
+
     Returns
     -------
-    str
-        The output of the shell command.
+    dict
+        A dictionary containing:
+        - 'stdout': The captured standard output.
+        - 'stderr': The captured standard error.
+        - 'return_code': The exit code of the command.
+
+    Raises
+    ------
+    CalledProcessError
+        If the command returns a non-zero exit code.
     """
-    output_str = os.popen(command_str).read()
-    return output_str
+    from subprocess import run, CalledProcessError
+    
+    # Execute the command and capture output
+    result = run(command, capture_output=capture_output, text=bool(encoding), shell=True)
+    
+    # Decode stdout/stderr if encoding is provided
+    stdout = result.stdout.strip()
+    stderr = result.stderr.strip()
+    
+    # Raise an error for non-zero return codes
+    if result.returncode != 0:
+        return dict(stdout=stdout, stderr=stderr, return_code=result.returncode)
+        raise CalledProcessError(result.returncode, command)
+    
+    return dict(stdout=stdout, stderr=stderr, return_code=result.returncode)
+
+
+# Auxiliary methods #
+#-------------------#
+
+def print_exit_info(process_exit_info_obj):
+    """
+    Print the exit information of a process.
+
+    This function checks the exit status of a process represented by the 
+    provided `process_exit_info_obj`. If the command string fails to execute,
+    it raises a RuntimeError indicating that the command was interpreted 
+    as a path.
+
+    Parameters
+    ----------
+    process_exit_info_obj : subprocess.CompletedProcess
+        An object containing the exit information of the process, 
+        typically returned by subprocess.run.
+
+    Raises
+    ------
+    RuntimeError
+        If the command string is interpreted as a path and fails to execute,
+        raising a FileNotFoundError.
+
+    Prints
+    ------
+    A message indicating whether the process completed successfully or 
+    details about the non-zero exit status, including the return code 
+    and any error message from stderr.
+    """
+    try:
+        process_exit_info_obj
+    except FileNotFoundError:
+        # If a str command fails, bash will usually interpret 
+        # that a path is trying to be searched,
+        # and if it fails to find, it will raise a Python-equivalent FileNotFoundError.
+        raise RuntimeError("Command string interpreted as a path. "
+                           "Please check the command.")
+    else:
+        return_code = process_exit_info_obj.returncode
+        if return_code == 0:
+            print("Process completed succesfully")
+        else:
+            err_arg_tuple = (return_code, process_exit_info_obj.stderr)
+            print_format_string(nonzero_exit_status_str, err_arg_tuple)
 
 
 #--------------------------#
 # Parameters and constants #
 #--------------------------#
 
-native_os_operation_modules = filesystem_context_modules[0::3]
+# Supported options #
+#-------------------#
+
+# Modules #
+system_command_modules = filesystem_context_modules[0::3]
+
+# Command run classes #
+class_list = ["system", "popen", "Popen", "call", "run"]
+
+
+# Preformatted strings #
+#----------------------#
+
+# Errors #
+nonzero_exit_status_str = """Process exited with status {} with the following error:\n{}"""
+
+# Switch case dictionaries #
+#--------------------------#
+
+# System command run helpers #
+command_helpers = {
+    ("os", "system"): os_system_helper,
+    ("os", "popen"): os_popen_helper,
+    ("subprocess", "Popen"): subprocess_popen_helper,
+    ("subprocess", "run"): subprocess_run_helper,
+    ("subprocess", "call"): subprocess_call_helper
+}
