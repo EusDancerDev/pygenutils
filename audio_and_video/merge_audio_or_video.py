b12371be1213a66988afe89e9f4c4469a31a769e
climatechange-tech <jagabantxo@gmail.com>
2024-09-25 13:42:18 +0200

    Add an external program that applies 'merge_audio_or_video' method of the module 'audio_and_video_manipulation'

---
+++ audio_and_video/merge_audio_or_video.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+**Note**
+
+This program is an application of the main module 'audio_and_video',
+and it relies on the method 'merge_audio_or_video_files'.
+YOU MAY REDISTRIBUTE this program along any other directory,
+but keep in mind that the module is designed to work with absolute paths.
+"""
+
+#-----------------------#
+# Import custom modules #
+#-----------------------#
+
+from pyutils.audio_and_video.audio_and_video_manipulation import merge_audio_or_video_files
+
+#-------------------#
+# Define parameters #
+#-------------------#
+
+# Simple data #
+#-------------#
+
+output_ext = "mp4"
+
+# Input media #
+#-------------#
+
+# List #
+media_file_list = []
+
+# External file containing file names #
+# media_name_containing_file = "media_name_containing_file.txt"
+
+# Output media #
+#--------------#
+
+# Merged media file #
+output_file_name = "output.{output_ext}"
+# output_file_name = None
+
+# Zero-padding and bit rate factor #
+"""The factor is multiplied by 32, so that the bit rate is in range [32, 320] kBps"""
+ZERO_PADDING = 1
+quality = 1
+
+#------------#
+# Operations #
+#------------#
+
+merge_audio_or_video_files(media_file_list,
+                           output_file_name=output_file_name,
+                           ZERO_PADDING=ZERO_PADDING,
+                           quality=quality)
+
+# merge_audio_or_video_files(media_name_containing_file,
+#                            output_file_name=output_file_name,
+#                            ZERO_PADDING=ZERO_PADDING,
+#                            quality=quality)
