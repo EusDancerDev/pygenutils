55a12c3a35d93b631aacf25f6b42dd1fc3607ba1
climatechange-tech <jagabantxo@gmail.com>
2024-09-26 10:25:09 +0200

    Substitute 'array_maths' module with the renamed 'maths'

--- weather_and_climate/consecutive_idx_statistics.py
+++ weather_and_climate/consecutive_idx_statistics.py
@@ -11,7 +11,7 @@ import numpy as np
 # Import custom modules #
 #-----------------------#
 
-from pyutils.arrays_and_lists.array_maths import count_consecutive, decompose_24h_cumulative_data
+from pyutils.arrays_and_lists.maths import count_consecutive, decompose_24h_cumulative_data
 
 #------------------#
 # Define functions #
