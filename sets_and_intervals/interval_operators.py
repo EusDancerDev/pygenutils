2fb68e318072f7135043a6d407eef93df8a97e67
climatechange-tech <jagabantxo@gmail.com>
2024-10-10 14:15:40 +0200

    Refactor interval operations to enhance flexibility and true union handling.
    - Moved switch-case logic for interval operations to external dictionaries for better code organization.
    
    - Added 'force_union' option for the 'pandas' constructor to perform true union, returning a single interval with the lowest left and highest right limits.
    
    - Addressed the behaviour of the 'piso' module where default union operations do not reflect a true union.

--- sets_and_intervals/interval_operators.py
+++ sets_and_intervals/interval_operators.py
@@ -5,7 +5,9 @@
 # Import modules #
 #----------------#
 
+import numpy as np
 import pandas as pd
+from intervaltree import Interval, IntervalTree
 
 #-----------------------#
 # Import custom modules #
@@ -21,112 +23,171 @@ from pyutils.utilities.instrospection_utils import get_caller_method_args
 #------------------#
 
 def define_interval(left_limit, right_limit, constructor="pandas", closed="both"):
+    """
+    Constructs an interval object using the specified constructor.
+
+    Parameters
+    ----------
+    left_limit : float or int
+        The left limit of the interval.
+    right_limit : float or int
+        The right limit of the interval.
+    constructor : str, optional
+        The library to use for constructing the interval. Options are:
+        'pandas', 'intervaltree', 'numpy', 'custom_tuple'. Default is 'pandas'.
+    closed : str, optional
+        Defines whether the interval is closed on the left, right, or both sides.
+        Valid values are 'left', 'right', 'both', or 'neither'. Default is 'both'.
+
+    Returns
+    -------
+    Interval object
+        An interval object based on the specified constructor.
+        
+    Raises
+    ------
+    ValueError
+        If an unsupported constructor is specified.
+
+    Examples
+    --------
+    >>> define_interval(1, 5, constructor="pandas", closed="both")
+    Interval(1, 5, closed='both')
+    """
     
-    # Input validation #
-    #------------------#
+    # Input validation # 
+    interval_contructor_options = ["pandas", "intervaltree", "numpy", "custom_tuple"]
     
-    # Main argument names and their position on the function's definition #    
-    all_arg_names = get_caller_method_args()    
-    contructor_arg_pos = find_substring_index(all_arg_names, "constructor")
-    
-    # constructor argument choice #    
-    if constructor not in contructor_arg_pos:
-        raise ValueError("Unsupported mathematical interval constructor library, "
-                         f"argument '{all_arg_names[contructor_arg_pos]}'. "
+    if constructor not in interval_contructor_options:
+        all_args = get_caller_method_args()
+        constr_arg_pos = find_substring_index(all_args, "constructor")
+        raise ValueError(f"Unsupported constructor '{constructor}' (position {constr_arg_pos}). "
                          f"Choose one from {interval_contructor_options}.")
-    
-    if constructor == "pandas":
-        import piso
-        piso.register_accessors()
-        itv = pd.Interval(left_limit, right_limit, closed=closed)
+
+    # Operations #
+    if constructor == "intervaltree":
+        print(f"WARNING: intervals constructed using constructor '{constructor}' "
+              f"do not include the upper bound.")
         
-    elif constructor == "intervaltree":
-        from intervaltree import Interval
-        print(f"WARNING: intervals constructed using {constructor} "
-              "do not include upper bound.")
-        itv = Interval(left_limit, right_limit)
-        
-    return itv
-    
+    return interval_constructors[constructor]()
+
 
 def basic_interval_operator(interval_array,
                             constructor="pandas",
-                            closed="left",
-                            operator="union", 
+                            closed="left", 
+                            operator="union",
                             force_union=False):
+    """
+    Performs operations on an array of interval objects using the specified constructor and operator.
+
+    Parameters
+    ----------
+    interval_array : array-like
+        Array of interval objects to perform operations on.
+    constructor : str, optional
+        The library used for constructing the intervals. Options are:
+        'pandas', 'intervaltree', 'numpy', 'custom_tuple'. Default is 'pandas'.
+    closed : str, optional
+        Defines whether the intervals are closed on the left, right, or both sides. 
+        Valid values are 'left', 'right', 'both', or 'neither'. Default is 'left'.
+    operator : str, optional
+        The operation to perform on the interval objects. Options are:
+        'union', 'intersection', 'difference', 'symmetric_difference', 'comparison'. Default is 'union'.
+    force_union : bool, optional
+        Forces the union of all intervals into a single interval if True. "
+        Only applies to 'union'. Default is False.
+
+    Returns
+    -------
+    Interval object or list of Interval objects
+        The result of the operation applied to the intervals.
+
+    Raises
+    ------
+    ValueError
+        If an unsupported constructor or operator is specified.
+
+    Examples
+    --------
+    >>> basic_interval_operator(interval_array, constructor="pandas", operator="union")
+    IntervalArray([...])
+
+    >>> basic_interval_operator(interval_array, constructor="intervaltree", operator="intersection")
+    IntervalTree([...])
+    """
     
     # Input validation #
     #------------------#
     
-    # Main argument names and their position on the function's definition #    
-    all_arg_names = get_caller_method_args()
-    operator_arg_pos = find_substring_index(all_arg_names, "operator_sets")
+    particular_constructor_opts = interval_contructor_options[:2]
     
-    # operator_sets and object type argument choices #    
-    if operator not in operations_sets_list:
-        raise ValueError("Invalid operator for mathematical sets, "
-                         f"argument '{all_arg_names[operator_arg_pos]}' option. "
-                         f"Supported options are {operations_sets_list}.")
+    if constructor not in particular_constructor_opts:
+        all_args = get_caller_method_args()
+        constr_arg_pos = find_substring_index(all_args, "constructor")
+        raise ValueError(f"Unsupported constructor '{constructor}' (position {constr_arg_pos}) "
+                         "for interval computations."
+                         f"Choose one from {particular_constructor_opts}.")
         
+    if operator not in operations_sets_list:
+        raise ValueError(f"Invalid operator '{operator}'. "
+                         f"Supported options are {operations_sets_list}.")
+
     # Operations #
     #------------#
     
-        
-    if constructor == "pandas":
-        import piso
-        piso.register_accessors()
-         
-        itv_pdArray = pd.arrays.IntervalArray(interval_array,
-                                              closed=closed)
-        
-        # TODO: garatu bost kasuak, denak web-orrialde ofizialetik
-        
-        if operator == "union":
-            merged_bin = itv_pdArray.piso.union()[0]
-            
-            if not force_union:
-                return merged_bin
-               
-            else:
-                merged_bin_left = merged_bin.left
-                merged_bin_right = merged_bin.right
-                
-                min_num_interval = itv_pdArray.min()
-                min_num_interval_left = min_num_interval.left
-                
-                max_num_interval = itv_pdArray.max()
-                max_num_interval_right = max_num_interval.right
-                
-                if merged_bin_left != min_num_interval_left\
-                or merged_bin_right != max_num_interval_right:
-
-                    merged_bin = define_interval(min_num_interval_left,
-                                                 max_num_interval_right,
-                                                 closed=closed)
-                    
-                    return merged_bin
-                    
-                else:
-                    return merged_bin
-                
-        elif operator == "intersection":
-            """do sth"""
-            
-    
-    elif constructor == "intervaltree":
-        print(f"WARNING: intervals constructed with method '{constructor}' "
-              "do not allow closed upper bounds.")        
-        from intervaltree import Interval, IntervalTree
-        itv_ItvArray = IntervalTree.from_tuples(interval_array)
-        
-        # TODO: garatu bost kasuak, denak web-orrialde ofizialetik
+    return interval_operations[constructor][operator](interval_array, closed, force_union)
 
 #--------------------------#
 # Parameters and constants #
 #--------------------------#
 
-# Operations with mathematical intervals #
-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-
+# Supported mathematical interval constructors and operations #
+interval_contructor_options = ["pandas", "intervaltree", "numpy", "custom_tuple"]
 
-# Supported mathematical interval constructors #
-interval_contructor_options = ["pandas", "intervaltree"]
+# Switch case dictionaries #
+#--------------------------#
+
+# Define interval constructors
+interval_constructors = {
+    "pandas": lambda left_limit, right_limit, closed : pd.Interval(left_limit, right_limit, closed=closed),
+    "intervaltree": lambda left_limit, right_limit, closed: Interval(left_limit, right_limit),
+    "numpy": lambda left_limit, right_limit, closed: np.array([left_limit, right_limit]),
+    "custom_tuple": lambda left_limit, right_limit, closed: (left_limit, right_limit)
+}
+
+# Define operations for pandas and intervaltree constructors
+operations_pandas = {
+    "union": lambda interval_array, closed, force_union: (
+        pd.arrays.IntervalArray(interval_array, closed=closed).piso.union()[0]
+        if not force_union else
+        define_interval(pd.arrays.IntervalArray(interval_array, closed=closed).left.min(),
+                        pd.arrays.IntervalArray(interval_array, closed=closed).right.max(), closed=closed)
+    ),
+    "intersection": lambda interval_array, closed, _: \
+        pd.arrays.IntervalArray(interval_array, closed=closed).piso.intersection()[0],
+    "difference": lambda interval_array, closed, _: \
+        pd.arrays.IntervalArray(interval_array, closed=closed).piso.difference()[0],
+    "symmetric_difference": lambda interval_array, closed, _: \
+        pd.arrays.IntervalArray(interval_array, closed=closed).piso.symmetric_difference()[0],
+    "comparison": lambda interval_array, closed, _: \
+        pd.arrays.IntervalArray(interval_array, closed=closed).piso.comparison()[0]
+}
+
+operations_intervaltree = {
+    "union": lambda interval_array, closed, _: \
+        IntervalTree.from_tuples(interval_array).merge_overlaps(),
+    "intersection": lambda interval_array, closed, _: \
+        IntervalTree.from_tuples(interval_array).overlap(),
+    "difference": lambda interval_array, closed, _: \
+        IntervalTree.from_tuples(interval_array).difference(),
+    "symmetric_difference": lambda interval_array, closed, _: \
+        IntervalTree.from_tuples(interval_array).symmetric_difference(),
+    "comparison": lambda interval_array, closed, _: \
+        IntervalTree.from_tuples(interval_array).comparison()
+}
+
+# Combine operations into a dictionary for easy access
+interval_operations = {
+    "pandas": operations_pandas,
+    "intervaltree": operations_intervaltree
+}
