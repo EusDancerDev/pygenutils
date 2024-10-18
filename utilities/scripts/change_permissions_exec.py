98224be440461a5b4d13e1095e25480d1aa1fba6
climatechange-tech <jagabantxo@gmail.com>
2024-10-18 15:44:01 +0200

    Adapt application program for file permission and owner modification
    - Divide inner code into more intuitive sections.
    - Removed deprecated `obj_type` argument, as the methods `modify_obj_permissions` and `modify_obj_owner` originally using it have been refined, and detect whether the object is a file or directory automatically.
    - Modify default attribute, permission and owner IDs to -1, meaning no changes are made.
    - Added in general notes the doc to refer to.

--- utilities/scripts/change_permissions_exec.py
+++ utilities/scripts/change_permissions_exec.py
@@ -9,66 +9,49 @@ and it uses the 'modify_obj_permissions' and 'modify_obj_owner'
 attributes and/or functions.
 YOU MAY REDISTRIBUTE this program along any other directory,
 but keep in mind that the module is designed to work with absolute paths.
+
+For more information about file object parameters, refer to the documentation
+of the module `permission_manager` (subpackage `file_operations` in `utilities`).
 """
 
 #-----------------------#
 # Import custom modules #
 #-----------------------#
 
-from pyutils.files_and_directories import change_permissions_main
-
-# Create aliases #
-#----------------#
-
-modify_obj_owner = change_permissions_main.modify_obj_owner
-modify_obj_permissions = change_permissions_main.modify_obj_permissions
+from pyutils.utilities.file_operations.permission_manager import modify_obj_owner, modify_obj_permissions
 
 #-------------------#
 # Define parameters #
 #-------------------#
 
+# File objects #
+#--------------#
+
+# Path to search for directories and files
 path = "/home/jonander/Documents"
 
-# Main task control switches #
-#----------------------------#
-
-make_obj_perm_mods = False
-make_obj_owner_mods = False
-
-# General parameters #
-obj_type = "file"
+# Extensions excluded from searching #
 extensions2skip = ""
 
-# Specific parameter oriented to object permission modification #
-attr_id = 644
+# File object properties #
+#------------------------#
+    
+# Permission ID #
+attr_id = -1
+    
+# Owner and group names or IDs 
+new_owner = -1
+new_group = -1
 
-# Specific parameters oriented to object owner modification #
+# Owner modification method params #
+#----------------------------------#
+
+# Module to use for #
 module = "shutil"
 
-"""
-**Note**
+#------------#
+# Operations #
+#------------#
 
-If there is no need to change the user and/or group name,
-please set one or both of the following to None or 'unchanged'.
-"""
-
-new_owner = None
-new_group = None
-
-#-------------------------------------------------------------#
-# Perform the tasks according to the values of the parameters #
-#-------------------------------------------------------------#
-    
-if make_obj_perm_mods:
-    modify_obj_permissions(path,
-                           obj_type,
-                           extensions2skip,
-                           attr_id)
-
-if make_obj_owner_mods:
-    modify_obj_owner(path,
-                     module,
-                     obj_type,
-                     extensions2skip,
-                     new_owner,
-                     new_group)
+modify_obj_permissions(path, extensions2skip, attr_id)
+modify_obj_owner(path, module, extensions2skip, new_owner, new_group)
