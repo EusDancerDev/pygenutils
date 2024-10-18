d62a5df3dc9de6181bb74577896bd9e765666dee
climatechange-tech <jagabantxo@gmail.com>
2024-10-18 13:02:44 +0200

    Adapt application program for scanning and reporting faulty netCDF files
    - Update absolute import: declare `scan_ncfiles` method from `file_utils` (`xarray_utils` subpackage), instead of the old-named `netcdf_file_scanner`.
    - General notes added:
    	- Designed for use with absolute paths and supports redistribution
    	- Program designed for use with absolute paths
    	- Supports redistribution

--- climate_data_utils/detect_faulty_ncfiles.py
+++ climate_data_utils/detect_faulty_ncfiles.py
@@ -1,42 +1,61 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+**Program Note**
+
+- This program is an application of the `scan_ncfiles` method from the 
+  `file_utils` module within the `xarray_utils` subpackage.
+- It is designed to scan specified directories for netCDF (`.nc`) files,
+  check their integrity if necessary, and generate a report of faulty files.
+- The `scan_ncfiles` method offers various configurations for returning 
+  file paths, directory paths, and integrity checks.
+
+**Redistribution Notice**
+You may redistribute this program in any other directory as needed.
+ However, keep in mind that it is designed to operate with absolute paths,
+ so ensure that any paths provided are properly configured to reflect your system's directory structure.
+
+**Main Functions and Subpackages Used**
+- `scan_ncfiles` (from `file_utils`, part of the `xarray_utils` subpackage):
+   The core function used to scan directories and check for `.nc` file integrity.
+- `ncfile_integrity_status` (from `file_utils`, part of the `xarray_utils` subpackage):
+   This function checks the integrity of `.nc` files and flags any issues.
+
+The execution of the program is timed using the `program_exec_timer` method 
+from the `time_handling` subpackage, which calculates the elapsed time of 
+the execution of the program for performance analysis.
+
+"""
+
 #-----------------------#
 # Import custom modules #
 #-----------------------#
 
-# TODO: eraiki txantiloia, databases/upload_data_to_mysql_database.py moduluan bezalatsu
-
 from pyutils.time_handling.program_snippet_exec_timers import program_exec_timer
-from pyutils.weather_and_climate.netcdf_handler import netcdf_file_scanner
+from pyutils.utilities.xarray_utils.file_utils import scan_ncfiles
 
 #-------------------#
 # Define parameters #
 #-------------------#
 
-# Path containing string or list of strings #
+# Paths to be scanned #
 path_obj = "/media/jonander/My_Basic/Dokumentuak"
 
-# path_obj = ["/media/jonander/My_Basic/Dokumentuak"
-#             "/home/jonander/Documents/03-Ikasketak]
+# path_obj = [
+#     "/media/jonander/My_Basic/Dokumentuak",
+#     "/home/jonander/Documents/03-Ikasketak"
+#     ]
 
-# Switch for scanning files only at the top level of the given path(s) #
-top_path_only = True
+#------------#
+# Operations #
+#------------#
 
-# Switch to print every file being scanned #
-extra_verbose = True
-
-#------------------------#
-# Initialise the program #
-#------------------------#
-
+# Initialise stopwatch #
 program_exec_timer('start')
-netcdf_file_scanner(path_obj, 
-                    top_path_only=top_path_only,
-                    extra_verbose=extra_verbose)
-    
-#---------------------------------------#
-# Calculate full program execution time #
-#---------------------------------------#
 
+# Run program #
+scan_ncfiles(path_obj)
+
+# Stop the stopwatch and calculate full program execution time #
 program_exec_timer('stop')
