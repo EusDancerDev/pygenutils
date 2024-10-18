3f24095180cec51848b31c0bd1851a52b6adbdae
climatechange-tech <jagabantxo@gmail.com>
2024-10-18 16:02:53 +0200

    Rename app program 'pdf_file_compressor_exec' to 'compress_pdf' (application program of the module 'file_format_tweaker' in 'utilities/file_operations' subpackage)

---
+++ utilities/scripts/compress_pdf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+**Note**
+
+This program is an application of the main module 'file_format_tweaker',
+and it uses the 'pdf_file_compressor' attributes and/or functions.
+YOU MAY REDISTRIBUTE this program along any other directory,
+but keep in mind that the module is designed to work with absolute paths.
+
+It is reccommended to firsly run this first cell, and then one of the 
+following cells, instead of running the whole program.
+"""
+
+#-----------------------#
+# Import custom modules #
+#-----------------------#
+
+from pyutils.files_and_directories.file_format_tweaker import pdf_file_compressor
+
+#-------------------#
+# Define parameters #
+#-------------------#
+
+#%%
+# 1st case usage #
+#----------------#
+
+in_path_str  = "/home/jonander/Documents/apunteak.pdf"
+out_path_str = "/home/jonander/Documents/tweaked.pdf"
+
+# For readability purposes, place the method here,
+# alongside with the case usage parameters 
+pdf_file_compressor(in_path_str, out_path_str)
+
+#%%
+# 2nd case usage #
+#----------------#
+
+in_path_list = ["/home/jonander/Documents/sample_1.pdf",
+                "/home/jonander/Documents/sample_2.pdf",
+                "/home/jonander/Documents/sample_3.pdf",
+                "/home/jonander/Documents/sample_4.pdf"]
+
+out_path_list = ["/home/jonander/Documents/compressed_sample1.pdf",
+                 "/home/jonander/Documents/compressed_sample2.pdf",
+                 "/home/jonander/Documents/compressed_sample3.pdf",
+                 "/home/jonander/Documents/compressed_sample4.pdf",]
+
+# For readability purposes, place the method here,
+# alongside with the case usage parameters 
+pdf_file_compressor(in_path_list, out_path_list)
