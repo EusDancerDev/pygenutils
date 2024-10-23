71f5fbb1a5ffaef07d96accca010781201832707
climatechange-tech <jagabantxo@gmail.com>
2024-10-23 19:08:06 +0200

    Substitute and relocate the new subpackage 'format_converters' in its absolute import

--- utilities/scripts/eml2pdf_exec.py
+++ utilities/scripts/eml2pdf_exec.py
@@ -5,7 +5,7 @@
 # Import custom modules #
 #-----------------------#
 
-from pyutils.files_and_directories.file_format_tweaker import eml2pdf
+from pyutils.utilities.format_converters.file_format_tweaker import eml2pdf
 
 #-------------------#
 # Define parameters #
