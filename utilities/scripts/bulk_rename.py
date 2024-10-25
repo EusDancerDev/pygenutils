bad0507027a84f440f38d85f2d67897a51c011e2
climatechange-tech <jagabantxo@gmail.com>
2024-10-25 19:11:18 +0200

    Revert "Delete old-named sub-package 'utilities' as it has been renamed"
    This reverts commit eaa0d1b590b8124b0b493abf82a98ec230734ef1.

---
+++ utilities/scripts/bulk_rename.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+**Note**
+
+This program is an application of the main module 'bulk_rename_auto',
+and it uses some of its attributes and/or functions.
+YOU MAY REDISTRIBUTE this program along any other directory,
+but keep in mind that the module is designed to work with absolute paths.
+"""
+
+#-----------------------#
+# Import custom modules #
+#-----------------------#
+
+from pyutils.utilities.file_operations.bulk_rename_auto import reorder_objs
+
+#-------------------#
+# Define parameters #
+#-------------------#
+
+path = "/home/jonander/Pictures/2023/Tenerife_test_rename_pyutils."
+obj_type = "file"
+
+zero_padding = 3
+extensions2skip = ""
+
+starting_number = "default"
+index_range = "all"
+
+splitdelim = None
+
+#------------------#
+# Perform the task #
+#------------------#
+
+reorder_objs(path,
+             obj_type,
+             extensions2skip,
+             index_range,
+             starting_number,
+             zero_padding,
+             splitdelim=splitdelim)
