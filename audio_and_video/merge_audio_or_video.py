9727a806967e4bf87976f566e7da1ee4d8adaa20
climatechange-tech <jagabantxo@gmail.com>
2025-03-23 13:01:03 +0100

    Enhance individual media merging script with dynamic file discovery and improved output handling
    - Updated variable name from `media_file_list` to `media_input` to better reflect its dual nature as either a list of media files or a single file containing media file names.
    - Changed the `output_file_list` variable to `output_file_name` to align with the purpose of the script.
    - Added commented lines to guide users on setting `media_input` and `output_file_name`:
      - `media_input` can be a list of files or a single file containing file names.
      - `output_file_name` can be a user-defined string or set to `None` for default naming.
    - Uppercase the `zero_padding` variable to `ZERO_PADDING` and increased quality setting to 4 for improved output quality.
    - Enhanced script flexibility and maintainability by replacing hardcoded lists with dynamic file handling.

--- audio_and_video/merge_audio_or_video.py
+++ audio_and_video/merge_audio_or_video.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# FIXME: artxibo honen izena, inportatzen den moduluaren ANTZEKOA da!
+# TODO: egokitu eta optimizatu alkartzeko pasatzen diren artxiboak zerrenda edo fitxategi moduan pasatzen direlako mekanismoak
+
+
 """
 **Note**
 
-This program is an application of the main module 'audio_and_video',
-and it relies on the method 'merge_audio_or_video_files'.
+This program is an application of the main module 'audio_and_video_manipulation',
+and it relies on the method 'merge_individual_media_files'.
 YOU MAY REDISTRIBUTE this program along any other directory,
 but keep in mind that the module is designed to work with absolute paths.
 """
@@ -13,7 +18,7 @@ but keep in mind that the module is designed to work with absolute paths.
 # Import custom modules #
 #-----------------------#
 
-from pygenutils.audio_and_video.audio_and_video_manipulation import merge_audio_or_video_files
+from pygenutils.audio_and_video.audio_and_video_manipulation import merge_individual_media_files
 
 #-------------------#
 # Define parameters #
@@ -27,34 +32,27 @@ output_ext = "mp4"
 # Input media #
 #-------------#
 
-# List #
-media_file_list = []
-
-# External file containing file names #
-# media_name_containing_file = "media_name_containing_file.txt"
+# Media input can be a list of files or a single file containing file names
+media_input = []
+# media_input = "media_name_containing_file.txt"
 
 # Output media #
 #--------------#
 
 # Merged media file #
-output_file_name = "output.{output_ext}"
+output_file_name = f"merged_media_file.{output_ext}"
 # output_file_name = None
 
 # Zero-padding and bit rate factor #
 """The factor is multiplied by 32, so that the bit rate is in range [32, 320] kBps"""
-zero_padding = 1
-quality = 1
+ZERO_PADDING = 1
+quality = 4
 
 #------------#
 # Operations #
 #------------#
 
-merge_audio_or_video_files(media_file_list,
-                           output_file_name=output_file_name,
-                           zero_padding=zero_padding,
-                           quality=quality)
-
-# merge_audio_or_video_files(media_name_containing_file,
-#                            output_file_name=output_file_name,
-#                            zero_padding=zero_padding,
-#                            quality=quality)
+merge_individual_media_files(media_input,
+                             output_file_name=output_file_name,
+                             ZERO_PADDING=ZERO_PADDING,
+                             quality=quality)
