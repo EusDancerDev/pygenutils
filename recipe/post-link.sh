2c0d13a2debcc99e2c2dc8fa26936b8d5fdce0d8
EusDancerDev <jagabantxo@gmail.com>
2025-07-28 10:33:56 +0200

    Update minimum recommended version of package 'filewise'
    - Update the minimum recommended version of `filewise` from 3.11.0 to is 3.11.6.
    - The reason is that since version 3.11.0, there have been up to six patch version number upgrades,
    which means that there are enough updates to consider the changes minimally breaking.

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-"${PREFIX}/bin/pip" install more_itertools>=10.0.0 filewise>=3.11.0 paramlib>=3.4.10 
+"${PREFIX}/bin/pip" install more_itertools>=10.0.0 filewise>=3.11.6 paramlib>=3.4.10 
