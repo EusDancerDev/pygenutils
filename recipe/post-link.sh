5cfd7262807dd382b6f0ef9ff4da494af0f3329b
EusDancerDev <jagabantxo@gmail.com>
2025-07-28 09:37:39 +0200

    Update minimum recommended versions of packages 'filewise' and 'paramlib'
    The minimum recommended versions of `filewise` and `paramlib` are 3.11.0 and 3.4.10, respectively

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-"${PREFIX}/bin/pip" install more_itertools>=10.0.0 filewise>=3.9.1 paramlib>=3.4.0 
+"${PREFIX}/bin/pip" install more_itertools>=10.0.0 filewise>=3.11.0 paramlib>=3.4.10 
